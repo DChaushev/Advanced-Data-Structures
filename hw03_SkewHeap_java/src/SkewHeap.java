e8a08343a4b0933fd7eb30c88c4928c146cf79b0
Dimitar Chaushev <d.chaushev073@gmail.com>
2015-03-22 22:58:46 +0200

    Skew Heap: looks like add() is working

---
+++ hw03_SkewHeap_java/src/SkewHeap.java
@@ -0,0 +1,148 @@
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Dimitar
+ *
+ * Data type that represents a min-heap implementation of Skew heap data
+ * stucture
+ */
+public class SkewHeap {
+
+    /**
+     * Reference to the root for the current skew heap
+     */
+    private Node root;
+
+    /**
+     * Data type that represents a node in the Skew heap
+     */
+    private static class Node {
+
+        int value; // data value
+        Node left; // left subtree
+        Node right; // right subtree
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public SkewHeap() {
+    }
+
+    /**
+     * Method that adds a node with data provided by @value in the current skew
+     * heap.
+     *
+     * @param value: value to be added in the heap
+     */
+    public void add(int value) {
+        this.root = merge(this.root, new Node(value));
+    }
+
+    /**
+     * This method removes and returns the smallest element in the current skew
+     * heap.
+     *
+     * @returns the removed element
+     *
+     * @throws Exception if there are no elements, but minimum was tried to be
+     * removed
+     */
+    public int removeMin() {
+        // Implement me...
+        return -1;
+    }
+
+    /**
+     * Tests whether there are any elements in the current heap.
+     *
+     * @returns true, if there are any elements and false, otherwise
+     */
+    public boolean empty() {
+        // Implement me...
+        return false;
+    }
+
+    /**
+     * Method that merges the current skew heap with the given by @other. This
+     * method destructs the @other skew heap while merging it.
+     *
+     * @param other : reference to the skew heap data structure that will be
+     * merged with the current one
+     */
+    public void merge(SkewHeap other) {
+        // Implement me...
+    }
+
+    /**
+     * Method that merges two skew heap data structures referenced to their
+     * roots by @root1 and @root2
+     *
+     * @param root1: reference to the root of the first skew heap
+     *
+     * @param root2: reference to the root of the second skew heap
+     *
+     * @returns a reference to the root of the merged data structure
+     */
+    private Node merge(Node root1, Node root2) {
+
+        Node firstRoot = root1;
+        Node secondRoot = root2;
+
+        if (firstRoot == null) {
+            return secondRoot;
+        } else if (secondRoot == null) {
+            return firstRoot;
+        }
+
+        if (firstRoot.value < secondRoot.value) {
+            Node temp = firstRoot.right;
+            firstRoot.right = firstRoot.left;
+            firstRoot.left = merge(secondRoot, temp);
+            return firstRoot;
+        } else {
+            return merge(secondRoot, firstRoot);
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append("===============\n");
+
+        Queue<Node> q = new ConcurrentLinkedQueue<>();
+
+        if (root != null) {
+            q.add(root);
+        }
+
+        while (!q.isEmpty()) {
+            Node n = q.peek();
+
+            q.remove();
+            result.append(String.format("%d:  ", n.value));
+            if (n.left != null) {
+                result.append(String.format(" left: %d ", n.left.value));
+                q.add(n.left);
+            }
+            if (n.right != null) {
+                result.append(String.format(" right: %d ", n.right.value));
+                q.add(n.right);
+            }
+
+            result.append("\n");
+        }
+
+        return result.toString();
+    }
+}
